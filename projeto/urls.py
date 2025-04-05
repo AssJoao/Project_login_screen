86e9156df9b472c2ebfcdec8cdf58dd1d0b3caee
AssJoao <jpaor.pedro@gmail.com>
2025-04-05 13:04:03 -0300

    Some changes

--- projeto/urls.py
+++ projeto/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from contas.views import home  # importa a view home
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  # Painel de administração do Django
-    path('contas/', include('contas.urls')),  # Rota para o app 'contas'
+    path('admin/', admin.site.urls),
+    path('', home, name='home'),  # ← isso faz a raiz do site carregar a home
+    path('contas/', include('contas.urls')),
 ]
